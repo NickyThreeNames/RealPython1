ccd582be1c8112f9217b3e43d9cc8ba43beee083
NickyThreeNames <nich.cm@gmail.com>
2015-04-30 19:54:47 +0000

    Add chapter 5 exercises

---
+++ Book1/Chapter5/5-1Review.py
@@ -0,0 +1,20 @@
+# Store an integer as a string
+my_integer = "5"
+
+# Convert the 'integer' string into an int object using int()
+# Multiply the integer by 7 and display the result
+print int(my_integer) * 7
+# Store a floating-point number as a string
+
+my_float = "5.1"
+
+# Convert the 'float' string into a number using float()
+# Multiply the number by 7 and display the result
+print float(my_float)*7
+
+# Create a string and an int object, then display them together
+my_int2 = 7
+my_string = "woo"
+print my_int2 + my_string
+
+#These do not work in Python 3.4 but work in 2.7
