0c05c127d9d3a7690339546217547f9692ce2771
NickyThreeNames <nich.cm@gmail.com>
2015-05-22 16:12:27 +0000

    Finished 5-3 review and Chapter 5 Assignment.

--- Book1/Chapter5/5-3Review.py
+++ Book1/Chapter5/5-3Review.py
@@ -1,5 +1,11 @@
 #Display result of find() on "a" in "AAA"
 print("AAA".find("a"))
 
-#Create string object
+#Create string object and subset 2.0 via a float variable
 s1 = "version 2.0"
+s2 = 2.0
+print(s1.find(str(s2)))
+
+#Take user input and find certain letter (a) in that input
+user_in = input("Type a word: ")
+print(user_in.find("a"))
---
+++ Book1/Chapter5/chapter5assign.py
@@ -0,0 +1,13 @@
+#Create l33t translator
+
+to_translate = input("Input text to translate: ")
+
+to_translate = to_translate.replace("a", "4")
+to_translate = to_translate.replace("b", "8")
+to_translate = to_translate.replace("e", "3")
+to_translate = to_translate.replace("l", "1")
+to_translate = to_translate.replace("o", "0")
+to_translate = to_translate.replace("s", "5")
+to_translate = to_translate.replace("t", "7")
+
+print(to_translate)
